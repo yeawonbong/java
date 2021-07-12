2efd7ae1c48a2c32ccacdf603ce4ba68e35f84bb
yeawonbong <ybong.develop@gmail.com>
2021-07-12 16:20:57 +0900

    java_day2

---
+++ Data_and_operation/Number.java
@@ -0,0 +1,16 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// Operator
+		System.out.println(6 + 2); // 8
+		System.out.println(6 - 2); // 4
+		System.out.println(6 * 2); // 12
+		System.out.println(6 / 2); // 3
+		
+		// 캐비넷 (library) - Math
+		System.out.println(Math.PI); //3.14159265..
+		System.out.println(Math.floor(Math.PI)); //floor는 내림이라는 뜻. --> 3.0
+		System.out.println(Math.ceil((Math.PI))); //ceil은 올림이라는 뜻. --> 4.0
+	}
+}
---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,14 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String (문자열)
+		System.out.println('H'); // Character (문자)
+		
+		System.out.println("plus " + "test");
+		System.out.println("enter\ntest");
+		System.out.println("Hello\"World\"");
+	}
+
+}
